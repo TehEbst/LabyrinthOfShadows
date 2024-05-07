8704a6884e98a2160b9d5b9ac2e3d03bee3b25f3
Brayden Medina <medinabrayden@utexas.edu>
2024-05-07 15:32:58 -0500

    Continuing converting and optimizing maze_generation

--- maze_generation.py
+++ maze_generation.py
@@ -1,9 +1,9 @@
 """
-This file contains the maze generation function
+This file contains the maze generation class
 """
 
 import random
-import numpy
+import numpy as np
 
 NORTH, EAST, SOUTH, WEST = 1, 2, 3, 4
 
@@ -26,6 +26,123 @@ class GenerateMaze:
         self.num_cols = cols
         self.maze = np.zeros([self.num_rows, self.num_cols, 5], int)
 
+    def mark_cell_visited(self, row: int, col: int):
+        # 0 indicates no visit, and 1 means it has been visited
+        self.maze[row, col, 0] = 1
+
+    def update_relation_state(self, row: int, col: int,
+                              direction: int, state: int):
+        """This will update status between current cell and neighbor in
+        specified direction"""
+
+        def update_neighbor_relation(row: int, col: int, direction: int, state: int):
+            # One way, update state in direction
+            self.maze[row, col, direction] = state
+
+        # Check for impossible assignments
+        if (row == 0 and direction == NORTH) or \
+           (col == self.num_cols - 1 and direction == EAST) or \
+           (row == self.num_rows - 1 and direction == SOUTH) or \
+           (col == 0 and direction == WEST):
+            return
+
+        # Update the relation in the current cell
+        update_neighbor_relation(row, col, direction, state)
+        # Update the relation in the neighbor cell
+        update_neighbor_relation(
+            *self.get_neighbor_coordinates(row, col,
+                                           direction, True), 2)
+        return
+
+    def get_neighbor_coordinates(self, row: int, col: int, neighbor_direction: int,
+                                 include_calling_cell_direction=False):
+        """
+        This function returns neighbor coordinates in a tuple of
+        row, col. Tuple can include direction of calling cell if True
+        """
+
+        # Ensure neighbor exists in that direction
+        if (row == 0 and neighbor_direction == NORTH) or \
+                (row == self.num_rows - 1 and neighbor_direction == SOUTH) or \
+                (col == 0 and neighbor_direction == WEST) or \
+                (col == self.num_cols - 1 and neighbor_direction == EAST):
+            return None
+
+        # Find coordinates of neighbor in that direction
+        if neighbor_direction == NORTH:
+            if include_calling_cell_direction:
+                calling_cell_direction = NORTH + 2
+                return row - 1, col, calling_cell_direction
+            return row - 1, col
+        elif neighbor_direction == EAST:
+            if include_calling_cell_direction:
+                calling_cell_direction = EAST + 2
+                return row, col + 1, calling_cell_direction
+            return row, col + 1
+        elif neighbor_direction == SOUTH:
+            if include_calling_cell_direction:
+                calling_cell_direction = SOUTH - 2
+                return row + 1, col, calling_cell_direction
+            return row + 1, col
+        elif neighbor_direction == WEST:
+            if include_calling_cell_direction:
+                calling_cell_direction = WEST - 2
+                return row, col - 1, calling_cell_direction
+            return row, col - 1
+
+    def get_unvisited_neighbor_direction(self, row: int, col: int):
+        # Returns directions of unvisited neighbors in a list
+        unvisited_list = []
+
+        # Create a list of all neighbors
+        for i in range(1, 5):
+            if self.maze[row, col, i] == 1:
+                neighbor_coordinates = self.get_neighbor_coordinates(row, col, i)
+                if self.maze[(*neighbor_coordinates, 0)] == 0:
+                    unvisited_list.append(i)
+
+        if not unvisited_list:
+            return None
+        else:
+            return random.choice(unvisited_list)
+
+    def generate_blank_maze(self):
+        # Generates a grid represented by a 3D matrix
+        for row in range(0, self.num_rows):
+            for column in range(0, self.num_cols):
+                if row < self.num_rows - 1:
+                    self.maze[row, column, SOUTH] = 1
+                if column < self.num_cols - 1:
+                    self.maze[row, column, EAST] = 1
+                if row > 0:
+                    self.maze[row, column, NORTH] = 1
+                if column > 0:
+                    self.maze[row, column, WEST] = 1
+
+    def inebriated_stroll(self, row, col):
+        """
+        This function starts at inputted cell, checks for unvisited neighbors,
+        connects the two, and marks them both as visited. Program terminates
+        when no unvisited cells are reachable
+        """
+
+        # Mark the current cell as visited
+        self.mark_cell_visited(row, col)
+
+        # Choose a random unvisited neighbor direction
+        unv_neighbor = self.get_unvisited_neighbor_direction(row, col)
+
+        # If no unvisited neighbors exist, return
+        if not unv_neighbor:
+            return
+
+        # Connect the two cells together
+        self.update_relation_state(row, col, unv_neighbor, 2)
+
+        # Get coordinates of new connection and call function again
+        unv_neighbor_coords = self.get_neighbor_coordinates(row, col, unv_neighbor)
+        self.inebriated_stroll(*unv_neighbor_coords)
+
     def hunt_and_kill(self, start=False):
         """
         This function implements the hunt and kill algorithm to
@@ -36,3 +153,18 @@ class GenerateMaze:
         stroll. We repeat this process until every cell has
         been visited
         """
+
+        def get_random_cell():
+            # Returns coordinates of a random cell
+            rand_row = random.randint(0, self.num_rows - 1)
+            rand_col = random.randint(0, self.num_cols - 1)
+            return rand_row, rand_col
+
+        if start:
+            self.generate_blank_maze()
+            self.inebriated_stroll(*get_random_cell())
+
+
+maze = GenerateMaze(3, 3)
+maze.hunt_and_kill(True)
+print(maze.maze)
---
+++ testing.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+x = 1, 0
+z = 1
+
+yuh = np.zeros((2, 2, 2))
+yuh[x] = 6
+
+print(yuh)
